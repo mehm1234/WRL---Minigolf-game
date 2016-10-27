97b751329516c914f370ac81fc256e124d151ea0
MichalSobcza <msobczak1994@gmail.com>
2016-10-27 15:31:23 +0100

    Initial upload
    First v of minigolf

---
+++ WRL - Minigolf/Minigolf/Minigolf/LoadOBJ.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+class Model_OBJ
+{
+public:
+	Model_OBJ();
+	float* Model_OBJ::calculateNormal(float* coord1, float* coord2, float* coord3);
+	int Model_OBJ::Load(char *filename);	// Loads the model
+	void Model_OBJ::Draw();					// Draws the model on the screen
+	void Model_OBJ::Release();				// Release the model
+
+	float* normals;							// Stores the normals
+	float* Faces_Triangles;				// Stores the triangles
+	float* vertexBuffer;					// Stores the points which make the object
+	long TotalConnectedPoints;				// Stores the total number of connected verteces
+	long TotalConnectedTriangles;			// Stores the total number of connected triangles
+
+};
